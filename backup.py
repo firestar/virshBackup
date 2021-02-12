97819672f7f68230530976d0408e5f77789030d3
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2021-02-11 20:00:55 -0800

    Create backup.py

---
+++ backup.py
@@ -0,0 +1,75 @@
+import os
+import re
+from datetime import datetime
+
+def local_command(command):
+    print "\t{ "+command + " } "
+    stream = os.popen(command)
+    output = stream.read()
+    return output
+
+def remote_command(command):
+    print "\t{ "+command + " } "
+    stream = os.popen('ssh '+HOST+' "'+command+'"')
+    output = stream.read()
+    return output
+
+HOST = "root@10.0.0.21";
+DIRECTORY = "/a/backups/"
+TEMP_DIRECTORY = "/temp/"
+
+VM_LIST = remote_command('virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf list --name').split("\n")
+for VM in VM_LIST:
+    if VM != "" and VM != "HostedEngine" and VM=="kafka":
+        current_timestamp = datetime.now().strftime("%Y-%j.%H%M%S")
+        path_to_backup = DIRECTORY + VM + "/" + current_timestamp + "/"
+
+        disks = []
+
+        print "Grabbing information on " + VM + " on " + current_timestamp + " saving to " + path_to_backup
+        print ""
+        print local_command('mkdir -p ' + path_to_backup)
+        print remote_command('mkdir -p ' + TEMP_DIRECTORY)
+        print "Created save path."
+        print ""
+        #print shell_command('ssh '+HOST+' virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf dominfo '+VM)
+        blocks=remote_command('virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf domblklist '+VM+' --details').split("\n")
+        for x in range(1, len(blocks)-1):
+            items = blocks[x].split()
+            if len(items) == 4 and items[1]=='disk':
+                qemu_info_list=remote_command('qemu-img info '+items[3]).split("\n")
+                qemu = {}
+                for qemu_info in qemu_info_list:
+                    match = re.search("([a-zA-Z ]+): (.*)", qemu_info)
+                    if match:
+                        qemu[match.group(1)] = match.group(2)
+
+                if qemu.has_key('file format'):
+                    disk_information = {
+                        "dev": items[2],
+                        "path": items[3],
+                        "type": qemu['file format']
+                    }
+                    disks.append(disk_information)
+                else:
+                    print "error qemu info check failed"
+        print disks
+        print ""
+        driver_string = []
+        for disk in disks:
+            driver_string.append('--diskspec '+disk['dev']+',driver=qcow2,file='+TEMP_DIRECTORY+VM+'-'+disk['dev']+'.cow')
+
+        print remote_command('virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf snapshot-create-as '+VM+' --no-metadata --disk-only --atomic '+" ".join(driver_string))
+
+        for disk in disks:
+            print remote_command('qemu-img convert -f raw -O qcow2 '+disk['path']+' '+TEMP_DIRECTORY+VM+'-'+disk['dev']+'-base.cow')
+            result_of_pivot = remote_command('virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf blockcommit '+VM+' '+disk['dev']+' --active --verbose --pivot')
+            if re.search("Successfully pivoted", result_of_pivot):
+                print result_of_pivot
+                print remote_command('rm -f '+TEMP_DIRECTORY+VM+'-'+disk['dev']+'.cow')
+            else:
+                print result_of_pivot
+            print local_command('scp '+HOST+':'+TEMP_DIRECTORY+VM+'-'+disk['dev']+'-base.cow '+path_to_backup+VM+'-'+disk['dev']+'.cow')
+            print remote_command('rm -f '+TEMP_DIRECTORY+VM+'-'+disk['dev']+'-base.cow')
+        print ""
+        print ""
